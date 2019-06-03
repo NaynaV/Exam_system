a8303a9d6cf220855d6c5560363eb7986e8a5cfb
NaynaV <50931344+NaynaV@users.noreply.github.com>
2019-06-03 18:30:27 -0400

    [Nayna]Add FinalExam Class and declare Methods as well as getter and setter

--- src/main/java/com/exam/Exam/finalExam.java
+++ src/main/java/com/exam/Exam/finalExam.java
@@ -1,51 +1,82 @@
 package com.exam.Exam;
 
-public class finalExam extends subject
+import java.util.Arrays;
+
+public class finalExam extends Exam
 {
-        private float fianlTotalMarks;
-        private float finalObtainedMarks;
-        private float finalPercentage;
-        private String finalResult;
+    private int weightageMarks;
+    private float marksFinal[];
+    private float totalMarksFinal;
+    private float percentageFinal;
+    private String resultFinal;
 
-    public float getFianlTotalMarks() {
-        return fianlTotalMarks;
+
+    public float[] getMarksFinal() {
+        return marksFinal;
     }
 
-    public void setFianlTotalMarks(float fianlTotalMarks) {
-        this.fianlTotalMarks = fianlTotalMarks;
+    public void setMarksFinal(float[] marksFinal) {
+        this.marksFinal = marksFinal;
     }
 
-    public float getFinalObtainedMarks() {
-        return finalObtainedMarks;
+    public float getTotalMarksFinal() {
+        return totalMarksFinal;
     }
 
-    public void setFinalObtainedMarks(float finalObtainedMarks) {
-        this.finalObtainedMarks = finalObtainedMarks;
+    public void setTotalMarksFinal(float totalMarksFinal) {
+        this.totalMarksFinal = totalMarksFinal;
     }
 
-    public float getFinalPercentage() {
-        return finalPercentage;
+    public float getPercentageFinal() {
+        return percentageFinal;
     }
 
-    public void setFinalPercentage(float finalPercentage) {
-        this.finalPercentage = finalPercentage;
+    public void setPercentageFinal(float percentageFinal) {
+        this.percentageFinal = percentageFinal;
     }
 
-    public String getFinalResult() {
-        return finalResult;
+    public String getResultFinal() {
+        return resultFinal;
     }
 
-    public void setFinalResult(String finalResult) {
-        this.finalResult = finalResult;
+    public void setResultFinal(String resultFinal) {
+        this.resultFinal = resultFinal;
+    }
+
+    public int getWeightageMarks() {
+        return weightageMarks;
+    }
+
+    public void setWeightageMarks(int weightageMarks) {
+        this.weightageMarks = weightageMarks;
     }
 
     @Override
     public String toString() {
         return "finalExam{" +
-                "fianlTotalMarks=" + fianlTotalMarks +
-                ", finalObtainedMarks=" + finalObtainedMarks +
-                ", finalPercentage=" + finalPercentage +
-                ", finalResult='" + finalResult + '\'' +
+                "weightageMarks=" + weightageMarks +
+                ", marksFinal=" + Arrays.toString(marksFinal) +
+                ", totalMarksFinal=" + totalMarksFinal +
+                ", percentageFinal=" + percentageFinal +
+                ", resultFinal='" + resultFinal + '\'' +
                 '}';
     }
+/*  public void displayFinalexaminfo() {
+        System.out.println("-----Finalexam Info-----");
+
+        System.out.println("Exam Type :" + getExamType());
+        System.out.println("Exam Date :" + getExamDate());
+        System.out.println("Course Name :" + getCourseName());
+        System.out.println("Subjects :" + getSubjectsName());
+        System.out.println("Weighatge :" + getWeightageMarks());
+        System.out.println("Marks :" + getMarks());
+        System.out.println("Toatl Marks :" + getTotalMarks());
+        System.out.println("Percenatage :" + getPercentage());
+        System.out.println("Result :" + getResult());
+        System.out.println("Student Name " + getStudentName());
+
+
+    }*/
+
+
 }
