e0a94dbee2e9156d7fec087b23ce58e5187bd378
NaynaV <50931344+NaynaV@users.noreply.github.com>
2019-06-04 17:05:47 -0400

    [Nayna] add Subject and Department details

--- src/main/java/com/exam/Course/Department.java
+++ src/main/java/com/exam/Course/Department.java
@@ -1,9 +1,48 @@
 package com.exam.Course;
 
-public class Department
+import com.exam.IDisplay;
+
+public class Department implements IDisplay
 {
         private int departmentId;
         private String departmentName;
 
+        //addMethod()
+        //IDisplay()
 
+
+        public int getDepartmentId() {
+                return departmentId;
+        }
+
+        public void setDepartmentId(int departmentId) {
+                this.departmentId = departmentId;
+        }
+
+        public String getDepartmentName() {
+                return departmentName;
+        }
+
+        public void setDepartmentName(String departmentName) {
+                this.departmentName = departmentName;
+        }
+
+
+        @Override
+        public String toString() {
+                return "Department{" +
+                        "departmentId=" + departmentId +
+                        ", departmentName='" + departmentName + '\'' +
+                        '}';
+        }
+
+
+        @Override
+        public void Display()
+        {
+
+                System.out.println("----- Department Details ------");
+                System.out.println("Department ID : " +getDepartmentId());
+                System.out.println("Department Name : "+getDepartmentName());
+        }
 }
